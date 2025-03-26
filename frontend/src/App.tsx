742673af565441e53150d96c132a73a95a0575cd
Cadecraft <cadendotlee@gmail.com>
2025-03-26 14:41:32 -0700

    feat: basic homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,57 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function CourseBox(props: {courseName: string, courseDetail: string}) {
+  return (
+    <div className="coursebox">
+      <b>{props.courseName}</b>
+      <br />
+      {props.courseDetail}
+    </div>
+  );
+}
 
+function HomePage() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="organizer">
+        <div className="section">
+          <h2>Courses</h2>
+          <div className="courselist">
+            <CourseBox courseName="ICS 33 Winter 2025" courseDetail="Intermediate Programming with Python" />
+            <CourseBox courseName="I&C SCI 6D LEC A" courseDetail="Discret Math for Cs" />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
+}
+
+function DiscussionPage() {
+  return (
+    <>
+      <div className="organizer">
+      </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <>
+      <div className="upperbar">
+        <div>
+          <b>edreader</b>
+        </div>
+        <div>
+          signed in as
+        </div>
+      </div>
+      <HomePage />
+      <DiscussionPage />
+    </>
+  );
 }
 
 export default App
